6efb87e372bce54d5ed1b2461cd3149c002225bd
Patrique Legault <patrique.legault@gmail.com>
2021-04-23 12:50:27 -0400

    Adding fix for session and connection

--- sample-jms-app/core-sample-jms/src/main/java/io/github/vm/patlego/jms/samples/SimpleReceiver.java
+++ sample-jms-app/core-sample-jms/src/main/java/io/github/vm/patlego/jms/samples/SimpleReceiver.java
@@ -8,6 +8,7 @@ import javax.jms.Session;
 import javax.jms.TextMessage;
 
 import org.apache.karaf.shell.api.action.Action;
+import org.apache.karaf.shell.api.action.Argument;
 import org.apache.karaf.shell.api.action.Command;
 import org.apache.karaf.shell.api.action.lifecycle.Reference;
 import org.apache.karaf.shell.api.action.lifecycle.Service;
@@ -18,26 +19,43 @@ import org.slf4j.LoggerFactory;
 @Command(scope = "examples", name = "consume", description = "Consume a message from a JMS queue")
 public class SimpleReceiver implements Action {
 
+    @Argument(index = 0, name = "queue", description = "Name of the queue", required = true, multiValued = false)
+    String queue;
+
     Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @Reference
+    @Reference(filter = "(name=patlego-vm-jms-sample1)")
     ConnectionFactory connectionFactory;
 
     @Override
     public Object execute() throws Exception {
-        Connection connection = connectionFactory.createConnection();
-        connection.start();
-        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-        Destination destination = session.createQueue("test");
-        MessageConsumer consumer = session.createConsumer(destination);
-        TextMessage message = (TextMessage) consumer.receive(60000);
-        if (message == null) {
-            throw new IllegalStateException("No message received");
-        } else {
-            logger.info(message.getText());
+        Connection connection = null;
+        Session session = null;
+        String result = null;
+
+        try {
+            connection = connectionFactory.createConnection();
+            connection.start();
+            session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            Destination destination = session.createQueue(queue);
+            MessageConsumer consumer = session.createConsumer(destination);
+            TextMessage message = (TextMessage) consumer.receive(60000);
+            if (message == null) {
+                throw new IllegalStateException("No message received");
+            } else {
+                logger.info("Received a message from the JMS queue");
+                result = message.getText();
+            }
         }
-        connection.close();
-        return null;
+        finally {
+            if (session != null) {
+                session.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        }
+         return result;
     }
     
 }
--- sample-jms-app/core-sample-jms/src/main/java/io/github/vm/patlego/jms/samples/SimpleSender.java
+++ sample-jms-app/core-sample-jms/src/main/java/io/github/vm/patlego/jms/samples/SimpleSender.java
@@ -8,6 +8,7 @@ import javax.jms.Session;
 import javax.jms.TextMessage;
 
 import org.apache.karaf.shell.api.action.Action;
+import org.apache.karaf.shell.api.action.Argument;
 import org.apache.karaf.shell.api.action.Command;
 import org.apache.karaf.shell.api.action.lifecycle.Reference;
 import org.apache.karaf.shell.api.action.lifecycle.Service;
@@ -16,19 +17,39 @@ import org.apache.karaf.shell.api.action.lifecycle.Service;
 @Command(scope = "examples", name = "send", description = "Send a message to a JMS queue")
 public class SimpleSender implements Action {
 
-    @Reference
+    @Argument(index = 0, name = "queue", description = "Name of the queue", required = true, multiValued = false)
+    String queue;
+
+    @Argument(index = 1, name = "message", description = "Message payload to send", required = true, multiValued = false)
+    String message;
+
+
+    @Reference(filter = "(name=patlego-vm-jms-sample1)")
     ConnectionFactory connectionFactory;
 
     @Override
     public Object execute() throws Exception {
-        Connection connection = connectionFactory.createConnection();
-        connection.start();
-        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-        Destination destination = session.createQueue("test");
-        MessageProducer producer = session.createProducer(destination);
-        TextMessage textMessage = session.createTextMessage("Hello from Pat!!!");
-        producer.send(textMessage);
-        connection.close();
+        Connection connection = null;
+        Session session = null;
+        try {
+            connection = connectionFactory.createConnection();
+            connection.start();
+            session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            Destination destination = session.createQueue(queue);
+            MessageProducer producer = session.createProducer(destination);
+            TextMessage textMessage = session.createTextMessage(message);
+            producer.send(textMessage);
+            session.close();
+            connection.close();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        }
+
         return null;
     }
 
